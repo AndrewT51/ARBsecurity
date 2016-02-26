6d46b5aa69dcfd0351c7855834b3aaffca48e6b7
Andrew Trigg <andrewt51@hotmail.com>
2016-02-26 21:50:20 +0000

    Basic functionality

---
+++ main.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var app = angular.module("securityApp",["ui.router"]);
+
+app.config(["$urlRouterProvider", "$stateProvider", "$locationProvider" ,function($urlRouterProvider,$stateProvider, $locationProvider){
+  $urlRouterProvider.otherwise('/home')
+  
+  // $locationProvider.html5Mode({
+  //   enabled:true,
+  //   requireBase: false
+  // });
+
+
+  $stateProvider
+  .state("home",{
+    url:"/home",
+    templateUrl: "templates/home.html"
+    // controller: "mainCtrl"
+  })
+  .state("mission-statement",{
+    url:"/mission-statement",
+    templateUrl: "templates/mission.html"
+    // controller: "mainCtrl"
+  })
+  .state("about",{
+    url:"/about",
+    templateUrl: "templates/about.html"
+    // controller: "mainCtrl"
+  })
+  .state("alarm-response",{
+    url:"/alarm-response",
+    templateUrl: "templates/alarmResponse.html"
+    // controller: "mainCtrl"
+  })
+  .state("event-security",{
+    url:"/event-security",
+    templateUrl: "templates/eventSecurity.html"
+    // controller: "mainCtrl"
+  })
+  .state("close-protection",{
+    url:"/close-protection",
+    templateUrl: "templates/closeProtection.html",
+    controller: "mainCtrl"
+  })
+  .state("oil-and-gas",{
+    url:"/oil-and-gas",
+    templateUrl: "templates/oilAndGas.html",
+    controller: "mainCtrl"
+ })
+  .state("estate-security",{
+    url:"/estate-security",
+    templateUrl: "templates/estateSecurity.html",
+    controller: "mainCtrl"
+ })
+  .state("mobile-patrols",{
+    url:"/mobile-patrols",
+    templateUrl: "templates/mobilePatrols.html",
+    controller: "mainCtrl"
+ })
+   .state("vetting",{
+    url:"/vetting",
+    templateUrl: "templates/vetting.html",
+    controller: "mainCtrl"
+ })
+
+}])
+
+app.controller("mainCtrl", ["$scope", function($scope){
+  // data.sections.forEach(function(el){
+  //   el.headingWithBreak = el.heading.replace(/ /,"\n")
+  // })
+  $scope.menuItems = data.sections;
+
+}])
